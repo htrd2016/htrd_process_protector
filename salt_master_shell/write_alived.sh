05169bdcd86749ec9d06693e448d8b700b434534
siryang2006 <393121997@qq.com>
2016-12-16 19:55:35 +0800

    Update write_alived.sh

--- salt_master_shell/write_alived.sh
+++ salt_master_shell/write_alived.sh
@@ -1,3 +1,3 @@
 salt -G "os:redhat" cmd.run 'echo "live" > /tmp/htrd_minion_alived'
-salt -G "os:CentOS7" cmd.run 'echo "live" > /tmp/htrd_minion_alived'
+#salt -G "os:CentOS" cmd.run 'echo "live" > /tmp/htrd_minion_alived'
 salt -G "os:windows" cmd.run 'echo "live" > %temp%\htrd_minion_alived'
