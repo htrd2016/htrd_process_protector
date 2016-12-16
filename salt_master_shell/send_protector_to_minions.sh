a57234aed662aa6f07e99ef0e376f6b6693d2f3c
siryang2006 <393121997@qq.com>
2016-12-16 19:55:06 +0800

    Update send_protector_to_minions.sh

--- salt_master_shell/send_protector_to_minions.sh
+++ salt_master_shell/send_protector_to_minions.sh
@@ -1,7 +1,7 @@
 salt -G "os:redhat" cp.get_file salt://minion_protector/htrdMinionProtector-el5 /root/minion_protector/htrdMinionProtector-el5 makedirs=True
 salt -G "os:redhat" cmd.run_all "chmod +x /root/minion_protector/htrdMinionProtector-el5" 
 
-salt -G "os:CentOS" cp.get_file salt://minion_protector/htrdMinionProtector-el7 /root/minion_protector/htrdMinionProtector-el7 makedirs=True
-salt -G "os:CentOS" cmd.run_all "chmod +x /root/minion_protector/htrdMinionProtector-el7"
+#salt -G "os:CentOS" cp.get_file salt://minion_protector/htrdMinionProtector-el7 /root/minion_protector/htrdMinionProtector-el7 makedirs=True
+#salt -G "os:CentOS" cmd.run_all "chmod +x /root/minion_protector/htrdMinionProtector-el7"
 
 salt -G "os:windows" cp.get_file salt://minion_protector/htrdMinionProtector.exe C:\\minion_protector\\htrdMinionProtector.exe makedirs=True
